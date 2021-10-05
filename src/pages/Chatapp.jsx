f6ec62806e20f7ded15e6e6392b6041b59e6e900
duyanhvu <anh.vu@tekmedi.vn>
2021-10-05 17:58:57 +0700

    ok

--- src/pages/Chatapp.jsx
+++ src/pages/Chatapp.jsx
@@ -1,214 +1,265 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react'
-import './scss/chatapp.css'
-import { Layout, Menu, Breadcrumb, notification, Input, Tooltip, Form, Row, Col, Avatar } from 'antd';
-import { RollbackOutlined, UserOutlined, SmileOutlined, SendOutlined, HeatMapOutlined } from '@ant-design/icons';
-import { Link, useLocation } from 'react-router-dom';
-import io from 'socket.io-client';
+import React, { useEffect, useState } from "react";
+import "./scss/chatapp.css";
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  notification,
+  Input,
+  Tooltip,
+  Form,
+  Row,
+  Col,
+  Avatar,
+} from "antd";
+import {
+  RollbackOutlined,
+  UserOutlined,
+  SmileOutlined,
+  SendOutlined,
+  HeatMapOutlined,
+} from "@ant-design/icons";
+import { Link, useLocation } from "react-router-dom";
+import io from "socket.io-client";
 // import { useSelector } from 'react-redux';
-import Qs from 'qs'
+import Qs from "qs";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 export const Chatapp = () => {
   const [form] = Form.useForm();
-  const [collapsed, setCollapsed] = useState(false)
-  const [userList, setUserList] = useState([])
-  const [errorAcknow, setErrorAcknow] = useState(null)
-  const [rooms, setRoom] = useState(null)
-  const [userName, setUserName] = useState(null)
-  const [userId, setUserId] = useState(null)
-  const [sendMessage, setSendMessage] = useState(null)
-  const [sendLocation, setSendLocation] = useState(null)
-  const [receiverMessage, setReceiverMessage] = useState(null)
-  const [receiverArrayMessage, setReceiverArrayMessage] = useState([])
+  const [collapsed, setCollapsed] = useState(false);
+  const [userList, setUserList] = useState([]);
+  const [errorAcknow, setErrorAcknow] = useState(null);
+  const [rooms, setRoom] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [sendMessage, setSendMessage] = useState(null);
+  const [sendLocation, setSendLocation] = useState(null);
+  const [receiverMessage, setReceiverMessage] = useState(null);
+  const [receiverArrayMessage, setReceiverArrayMessage] = useState([]);
 
-  const queryString = useLocation()
+  const queryString = useLocation();
 
-  const PORRT = 'localhost:5000';
+  const PORRT = "localhost:5000";
   const socket = io(PORRT);
 
   useEffect(() => {
-    const { room, userName } = Qs.parse(queryString.search, { ignoreQueryPrefix: true })
-    setRoom(room)
-    setUserName(userName)
+    const { room, userName, email } = Qs.parse(queryString.search, {
+      ignoreQueryPrefix: true,
+    });
+    setRoom(room);
+    setUserName(userName);
+    setEmail(email);
     // join room
-    socket.emit('join room', { room, userName })
+    socket.emit("join room", { room, userName, email });
     // render list member
-    socket.on('send list client inside room', (userList) => {
-      setUserId(socket.id)
-      getListUser()
-    })
-    // semd message
-    socket.emit("send message", sendMessage, acknowLedGements)
+    socket.on("send list client inside room", (userList) => {
+      console.log(userList)
+      getListUser();
+    });
+    // send message
+    socket.emit("send message", sendMessage, acknowLedGements);
     // reciver message
-    socket.on('send message', async (receiverMessage) => {
-      await setReceiverMessage(receiverMessage)
-    })
-    socket.on('send array message', async (arrayMessage) => {
-      await setReceiverArrayMessage(arrayMessage)
-    })
+    socket.on("send message", (receiverMessage) => {
+      setReceiverMessage(receiverMessage);
+    });
+    socket.on("send array message", (arrayMessage) => {
+      setReceiverArrayMessage(arrayMessage);
+    });
     // send location
-    socket.emit('send location', sendLocation)
+    socket.emit("send location", sendLocation);
     // disconecet
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       return () => {
-        socket.disconnect()
-      }
-    })
-
-  }, [PORRT, queryString.search, sendMessage, sendLocation])
+        socket.disconnect();
+      };
+    });
+  }, [PORRT, queryString.search, sendMessage, sendLocation]);
 
   const getListUser = () => {
-    fetch('http://localhost:5000/api/v1/listUser')
+    fetch("http://localhost:5000/api/v1/listUser")
       .then((result) => {
         if (result.ok) {
-          return result.json()
+          return result.json();
         }
-        throw result
+        throw result;
       })
       .then((data) => {
-        setUserList(data)
+        setUserList(data);
       })
-      .catch(err => console.log("err", err))
-  }
+      .catch((err) => console.log("err", err));
+  };
 
   useEffect(() => {
-    // display notify
-    socket.on('send message notify', (message) => {
-      openNotificationJoin(message)
-    })
-  }, [])
+    socket.on("send message notify", (message) => {
+      openNotificationJoin(message);
+    });
+  }, []);
 
   useEffect(() => {
     if (errorAcknow !== null) {
-      openNotificationAcknow()
+      openNotificationAcknow();
     }
-  })
+  });
 
   useEffect(() => {
     if (receiverMessage && receiverMessage !== null) {
-      setErrorAcknow(null)
+      setErrorAcknow(null);
     }
-  })
+  });
 
   const openNotificationJoin = (notify) => {
     notification.open({
       message: `${notify}`,
-      icon: <SmileOutlined style={{ color: '#108ee9' }} />,
+      icon: <SmileOutlined style={{ color: "#108ee9" }} />,
     });
   };
 
   const openNotificationAcknow = () => {
     notification.open({
       message: `${errorAcknow}`,
-      icon: <SmileOutlined style={{ color: '#e91010' }} />
+      icon: <SmileOutlined style={{ color: "#e91010" }} />,
     });
   };
 
   const acknowLedGements = (error) => {
     if (error) {
-      setErrorAcknow(error)
+      setErrorAcknow(error);
     }
-  }
+  };
 
   const onSendMessage = (e) => {
     if (e.message !== "") {
-      setSendMessage(e.message)
+      setSendMessage(e.message);
     }
-  }
+  };
 
   const shareLocation = () => {
     if (!navigator.geolocation) {
-      return ('Browser Not Support Location')
+      return "Browser Not Support Location";
     }
     navigator.geolocation.getCurrentPosition((position) => {
       const location = {
         latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      }
-      setSendLocation(location)
-    })
-  }
+        longitude: position.coords.longitude,
+      };
+      setSendLocation(location);
+    });
+  };
 
   const renderMessage = () => {
     if (receiverArrayMessage) {
       return receiverArrayMessage.map((row, idx) => {
-        if (row.userId !== userId) {
+        if (row.email !== email) {
           return (
-            <div key={idx} className='memberChat'>
-              <div style={{ display: 'flex' }}>
-                <div className='avatar'><Avatar style={{ backgroundColor: '#87d068' }}>{row.userRoom}</Avatar></div>
-                <div className='chat'>{row.message}</div>
+            <div key={idx} className="memberChat">
+              <div style={{ display: "flex" }}>
+                <div className="avatar">
+                  <Avatar style={{ backgroundColor: "#87d068" }}>
+                    {row.userName}
+                  </Avatar>
+                </div>
+                <div className="chat">{row.message}</div>
               </div>
-              <p className='time'>{row.createAt}</p>
+              <p className="time">{row.createAt}</p>
             </div>
-          )
+          );
         }
         return (
-          <div key={idx} className='myChat'>
-            <div className='warpMyChat'>
-              <div style={{ display: 'flex' }}>
-                <div className='chat'>{row.message}</div>
-                <div className='avatar'><Avatar style={{ backgroundColor: '#87d068' }}>{row.userRoom}</Avatar></div>
+          <div key={idx} className="myChat">
+            <div className="warpMyChat">
+              <div style={{ display: "flex" }}>
+                <div className="chat">{row.message}</div>
+                <div className="avatar">
+                  <Avatar style={{ backgroundColor: "#87d068" }}>
+                    {row.userRoom}
+                  </Avatar>
+                </div>
               </div>
-              <p className='time'>{row.createAt}</p>
+              <p className="time">{row.createAt}</p>
             </div>
           </div>
-        )
-      })
+        );
+      });
     }
-  }
+  };
 
   const onCollapse = (collapsed) => {
-    setCollapsed(collapsed)
+    setCollapsed(collapsed);
   };
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: "100vh" }}>
       <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
         <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
           <Menu.Item key={userList.length + 1} icon={<RollbackOutlined />}>
-            <Link to='/'>Come Back</Link>
+            <Link to="/">Come Back</Link>
           </Menu.Item>
           <SubMenu key="sub1" icon={<UserOutlined />} title="Member">
-            {userList.map((item, index) => {
-              return (
-                <Menu.Item key={index}>{item.userName}</Menu.Item>
-              )
+            {userList.filter(item => item.userName === userName).map((row, idx) => {
+              return <Menu.Item key={idx}>{row.userName}</Menu.Item>;
             })}
           </SubMenu>
         </Menu>
       </Sider>
       <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }}>Room Chat : {rooms?.toUpperCase()}</Header>
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          Room Chat : {rooms?.toUpperCase()}
+        </Header>
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>
-              <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} /> <span className='account'>{userName}</span>
+              <Avatar
+                style={{ backgroundColor: "#87d068" }}
+                icon={<UserOutlined />}
+              />{" "}
+              <span className="account">{userName}</span>
             </Breadcrumb.Item>
           </Breadcrumb>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360, borderRadius: '10px' }}>
-            <Row className='rowChat'>
-              <Col span={24}>
-                {renderMessage()}
-              </Col>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360, borderRadius: "10px" }}
+          >
+            <Row className="rowChat">
+              <Col span={24}>{renderMessage()}</Col>
             </Row>
           </div>
-          <Form form={form} name="horizontal_login" onFinish={onSendMessage} style={{ padding: '20px 0', borderRadius: '5px' }}>
+          <Form
+            form={form}
+            name="horizontal_login"
+            onFinish={onSendMessage}
+            style={{ padding: "20px 0", borderRadius: "5px" }}
+          >
             <Form.Item name="message">
               <Input
                 placeholder="Enter Message"
-                prefix={<SendOutlined type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                prefix={
+                  <SendOutlined
+                    type="user"
+                    style={{ color: "rgba(0,0,0,.25)" }}
+                  />
+                }
                 suffix={
                   <React.Fragment>
                     <Tooltip title="Send Message" htmlType="submit">
-                      <SendOutlined type="info-circle" style={{ color: 'rgba(0,0,0,.45)', marginRight: '10px' }} />
+                      <SendOutlined
+                        type="info-circle"
+                        style={{
+                          color: "rgba(0,0,0,.45)",
+                          marginRight: "10px",
+                        }}
+                      />
                     </Tooltip>
                     <Tooltip title="Share Location" htmlType="submit">
-                      <HeatMapOutlined type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} onClick={shareLocation} />
+                      <HeatMapOutlined
+                        type="info-circle"
+                        style={{ color: "rgba(0,0,0,.45)" }}
+                        onClick={shareLocation}
+                      />
                     </Tooltip>
                   </React.Fragment>
                 }
@@ -216,9 +267,10 @@ export const Chatapp = () => {
             </Form.Item>
           </Form>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Vũ Duy Anh Design ©2021 Created Chat_App</Footer>
+        <Footer style={{ textAlign: "center" }}>
+          Vũ Duy Anh Design ©2021 Created Chat_App
+        </Footer>
       </Layout>
     </Layout>
-  )
-}
-
+  );
+};
